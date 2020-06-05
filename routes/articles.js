1dfa5d9d2945d1341568db4697065e8a4451091d
Daniel Brime <dbrime@gmail.com>
2020-06-04 22:01:49 -0400

    Adds fetch for articles.getAll

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,7 @@ const articles = express.Router();
 
 // Models
 const Article = require('../models/Article');
+const Hospital = require('../models/Hospital');
 
 //articles.use(middleware.isAuthenticated);
 
@@ -17,7 +18,28 @@ articles.route('/')
           return res.status(error.statusCode).json(error);
         }
 
-        return res.status(200).json(articles);
+        const hospitalIds = articles.map(article => article.hospitalId);
+        const uniqueHospitalIds = [ ...new Set(hospitalIds) ];
+
+        return Hospital.fetch(uniqueHospitalIds, (error, hospitals) => {
+          if (error) {
+            return res.status(error.statusCode).json(error);
+          }
+
+          articles = articles.map(article => {
+            let hospital = hospitals.find(hospital => hospital._id === article.hospitalId);
+
+            article.hospitalName = hospital.name;
+
+            return article;
+
+          })
+
+          return res.status(200).json(articles);
+
+        });
+
+
       });
     });
 
