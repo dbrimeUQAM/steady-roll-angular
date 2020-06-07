50e73b40517be7a653ed17b754ef9de4907c5e97
GhadaSou <46688476+GhadaSou@users.noreply.github.com>
2020-06-06 20:52:25 -0400

    fix sort and filter

--- src/app/articles/articles-list.component.ts
+++ src/app/articles/articles-list.component.ts
@@ -5,63 +5,34 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 
+
 @Component({
   selector: 'app-article-list',
   templateUrl: './articles-list.component.html',
   styleUrls: ['./articles-list.component.css']
 })
 export class ArticlesListComponent implements OnInit {
-
-  articles: Article[] = [];
-  columnsToDisplay: string[] = ['name', 'description', 'hospitalName', 'articleType'];
-  isLoadingResults = true;
+  displayedColumns: string[] = ['name', 'hospitalName', 'description', 'type'];
   dataSource: MatTableDataSource<Article>;
+  articles: Article[] = [];
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  constructor(private articleService: ArticleService) {
-    
-   }
+  constructor(private articleService: ArticleService ) {}
 
-  ngOnInit(): void {
-
+  ngOnInit() {
     this.articleService.getAll()
     .subscribe((res: any) => {
       this.articles = res;
       console.log(this.articles);
-      this.isLoadingResults = false;
+      this.dataSource = new MatTableDataSource(this.articles);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     }, err => {
       console.log(err);
-      this.isLoadingResults = false;
     });
-    this.dataSource = new MatTableDataSource(this.articles)
-    console.log(this.dataSource);
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-
+    
   }
- /*  sortData(sort: Sort) {
-    const data = this.articles;
-    if (!sort.active || sort.direction === '') {
-      this.articles = data;
-      return;
-    }
-
-    this.articles = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'description': return compare(a.description, b.description, isAsc);
-        case 'hospital': return compare(a.hospitalName, b.hospitalName, isAsc);
-        case 'type': return compare(a.articleType, b.articleType, isAsc);
-        default: return 0;
-      }
-    });
-    function compare(a: number | string, b: number | string, isAsc: boolean) {
-      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-    }
-  } */
-
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -71,6 +42,8 @@ export class ArticlesListComponent implements OnInit {
       this.dataSource.paginator.firstPage();
     }
   }
+
   
 
 }
+
