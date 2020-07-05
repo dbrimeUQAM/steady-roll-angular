f9979cc060125b60b84cfe12608d75c91c282277
MassekMeriam <43073225+MassekMeriam@users.noreply.github.com>
2020-07-05 11:24:22 -0400

    Update bag-page.component.ts

--- src/app/bag-page/bag-page.component.ts
+++ src/app/bag-page/bag-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ArticleService } from '../services/article/article.service';
+import { OrderService } from '../services/order/order.service';
 import { Article } from '../services/article/article';
 import { MatTableDataSource } from '@angular/material/table';
 import { FormBuilder, FormGroup } from '@angular/forms';
@@ -23,23 +23,19 @@ export class BagPageComponent implements OnInit {
   receivedChildMessage: number = 0;
 
 
-  constructor(private articleService: ArticleService, private formBuilder: FormBuilder) { }
+  constructor(private orderService: OrderService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.articleService.getAll()
+    this.orderService.getAll()
     .subscribe((res: any) => {
       this.articles = res;
       this.dataSource = new MatTableDataSource(this.articles);
+      console.log("orderService . get all");
+      console.log(this.articles);
     }, err => {
       console.log(err);
     },() =>{
       this.articleNum = this.articles.length ;
-     /*  this.articleService.getArticleById('a97de9be823399d1b7377d1a7abbf2e2').subscribe( data =>{
-        this.article = data
-      }, err =>{console.log(err);
-      },() =>{
-          //
-      }) */
     }
     );
   }
@@ -50,7 +46,7 @@ export class BagPageComponent implements OnInit {
     this.receivedChildMessage = message ? this.receivedChildMessage + message: 0 ;
     console.log('this.receivedChildMessage',this.receivedChildMessage)
   }
- 
+
 getTotal(): number{
   return this.receivedChildMessage ;
 }
