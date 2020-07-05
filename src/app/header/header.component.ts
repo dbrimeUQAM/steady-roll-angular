11cd82832ae2270a5057de892befb522b0327d2f
MassekMeriam <43073225+MassekMeriam@users.noreply.github.com>
2020-07-05 18:43:45 -0400

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenStorageService } from '../services/token-storage/token-storage.service';
-import { OrderService } from '../services/order/order.service';
-import { Article } from '../services/order/article';
 
 @Component({
   selector: 'app-header',
@@ -10,27 +8,18 @@ import { Article } from '../services/order/article';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  articles: Article[] = [];
   articleNum: Number = 0 ;
 
   name: string ;
   hospitalName: string ;
 
-  constructor(private orderService: OrderService, private router: Router, private tokenStorage: TokenStorageService) { }
+  constructor(private router: Router, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
     console.log('------>', this.tokenStorage.getUser() )
     this.name = this.tokenStorage.getUser().name;
     this.hospitalName= this.tokenStorage.getUser().hospital.name;
-
-    this.orderService.getAll(this.tokenStorage.getUser().id).subscribe(data => {
-      this.articles= data ;
-    }, err =>{
-      console.log(err);
-    },()=> {
-      this.articleNum = this.articles.length ;
-    }
-    )
+    this.articleNum = this.tokenStorage.getUser().order.articles.length ;
   }
   onClick($event) {
     this.tokenStorage.signOut();
