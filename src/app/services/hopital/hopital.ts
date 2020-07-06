5a61f1c012ef1b1204ed617228b2f78bf1596591
sana jaziri <ef891089@ens.uqam.ca>
2020-07-05 23:32:53 -0400

    Ajout class hopital

---
+++ src/app/services/hopital/hopital.ts
@@ -0,0 +1,12 @@
+export class Hospital {
+    _id: string;
+    hospitalType: string;
+    name: string;
+    phoneNumber: string;
+    street: string;
+    city: string;
+    province: string;
+    postalCode: string;
+    offerType: string;
+
+}
