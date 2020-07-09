7a0d2230fe3301d9b2a3e0fd68a73a4b3c7eaff4
sana jaziri <ef891089@ens.uqam.ca>
2020-07-09 00:56:41 -0400

    Ajout validation date, quantité et prix

--- src/app/add-article/add-article.component.ts
+++ src/app/add-article/add-article.component.ts
@@ -15,9 +15,10 @@ export class AddArticleComponent implements OnInit {
   article: Article;
   hospitalName: string;
   hospitalId: string;
+  minDate: Date;
 
   public articleFrom: FormGroup;
-  labelPosition: string; 
+  labelPosition: string;
   choix: string[] = ['donner', 'vendre'];
   name: boolean;
   type: boolean;
@@ -30,10 +31,14 @@ export class AddArticleComponent implements OnInit {
   constructor(private router: Router, private formBuilder: FormBuilder, private articleService: ArticleService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
+    const currentMonth = new Date().getMonth();
+    const currentYear = new Date().getFullYear();
+    const currentDay = new Date().getDay();
+    this.minDate = new Date(currentYear, currentMonth + 1, currentDay + 5);
     this.article = new Article;
     this.hospitalName = this.tokenStorage.getUser().hospital.name;
     this.hospitalId = this.tokenStorage.getUser().hospital._id;
-    this.article.hospitalId = this.hospitalId ;
+    this.article.hospitalId = this.hospitalId;
 
     this.articleFrom = this.formBuilder.group({
       articleType: ['', Validators.required],
@@ -42,8 +47,8 @@ export class AddArticleComponent implements OnInit {
       expirationDate: '',
       condition: ['', Validators.required],
       offerType: ['', Validators.required],
-      quantity: [0, Validators.required],
-      price: 0 ,
+      quantity: [0, [Validators.required, Validators.max(900)]],
+      price: [0, [Validators.required, Validators.max(900)]],
     })
     this.articleFrom.get('name').valueChanges.subscribe(data => {
       this.article.name = data;
@@ -71,33 +76,33 @@ export class AddArticleComponent implements OnInit {
     });
   }
   submit() {
-    if(this.articleFrom.get('name').invalid){
-      this.name = true ;
+    if (this.articleFrom.get('name').invalid) {
+      this.name = true;
     }
-    if(this.articleFrom.get('articleType').invalid){
-      this.type = true ;
+    if (this.articleFrom.get('articleType').invalid) {
+      this.type = true;
     }
-    if(this.articleFrom.get('description').invalid){
-      this.description = true ;
+    if (this.articleFrom.get('description').invalid) {
+      this.description = true;
     }
-    if(this.articleFrom.get('condition').invalid){
-      this.condition = true ;
+    if (this.articleFrom.get('condition').invalid) {
+      this.condition = true;
     }
-    if(this.articleFrom.get('quantity').invalid){
-      this.qty = true ;
+    if (this.articleFrom.get('quantity').invalid) {
+      this.qty = true;
     }
-    if(this.articleFrom.get('offerType').invalid){
-      this.mode = true ;
+    if (this.articleFrom.get('offerType').invalid) {
+      this.mode = true;
     }
-    
-    if(this.articleFrom.valid) {
+
+    if (this.articleFrom.valid) {
       this.articleService.addArticle(this.article).subscribe(data => {
         const id = data._id;
         this.router.navigate(['/home']);
-  
+
       })
     }
-   
+
   }
 }
 export class DatepickerCustomIconExample { }
