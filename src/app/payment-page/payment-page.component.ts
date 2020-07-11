ff12855f77f7549fe826a72f2396d738fbbf1015
GhadaSou <46688476+GhadaSou@users.noreply.github.com>
2020-07-10 23:47:38 -0400

    payement

--- src/app/payment-page/payment-page.component.ts
+++ src/app/payment-page/payment-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-page',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-page.component.css']
 })
 export class PaymentPageComponent implements OnInit {
+  payementForm: FormGroup ;
+  isMasterCard: boolean = false ;
+  isVisa: boolean = false ;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.payementForm = this.formBuilder.group ({
+      name: ''
+    })
+    this.payementForm.get('name').valueChanges.subscribe(
+      data => {
+        console.log('dataa changed', data)
+        if(data === '5' ){
+          this.isMasterCard = true;
+        }else if (data === '4') {
+          this.isVisa= true ;
+        }else{
+          this.isVisa= false ;
+          this.isMasterCard = false;
+        }
+      }
+    ),err => {
+      console.log(err)
+    }
   }
 
 }
