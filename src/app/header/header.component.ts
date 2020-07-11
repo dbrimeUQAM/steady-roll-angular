c0761e30c7c5b82204962b44f083dc625c0c067c
GhadaSou <46688476+GhadaSou@users.noreply.github.com>
2020-07-11 12:25:15 -0400

    fix articles number in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,9 @@ import { Router } from '@angular/router';
 import { TokenStorageService } from '../services/token-storage/token-storage.service';
 
 import { MatSidenav } from '@angular/material/sidenav';
+import { User } from '../services/user/user';
+import { OrderService } from '../services/order/order.service';
+import { Article } from '../services/article/article';
 
 @Component({
   selector: 'app-header',
@@ -15,15 +18,29 @@ export class HeaderComponent implements OnInit {
   name: string;
   articleNum = 0;
   hospitalName: string;
+  user: User;
+  articles: Article[]=[];
 
-  constructor( private router: Router, private tokenStorage: TokenStorageService ) { }
+  constructor(private orderService:OrderService, private router: Router, private tokenStorage: TokenStorageService ) { }
 
   ngOnInit(): void {
     this.inputSideNav.open();
-    const user = this.tokenStorage.getUser();
-    this.name = user.name;
-    this.hospitalName = user.hospital ? user.hospital.name : '';
-    this.articleNum = user.order ? user.order.articles.length : 0 ;
+    this.user = this.tokenStorage.getUser().hospital ;
+    console.log('user dans header', this.tokenStorage.getUser())
+    this.name = this.user.name;
+    this.hospitalName = this.tokenStorage.getUser().hospital ? this.tokenStorage.getUser().hospital.name : '';
+    this.articleNum = this.tokenStorage.getUser().hospital.order ? this.tokenStorage.getUser().hospital.articles.length : 0 ;
+    this.orderService.getCurrentOrder(this.tokenStorage.getUser().id)
+    .subscribe((res: any) => {
+      console.log('my order', res)
+      this.articles = res.articles;
+    }, err => {
+      console.log(err);
+    }, () => {
+      this.articleNum = this.articles.length ;
+    })
+    console.log('number articles', this.articleNum)
+
   }
 
   onClick($event) {
