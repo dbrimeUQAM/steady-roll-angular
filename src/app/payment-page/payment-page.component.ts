46479209e31cdaa57d7d74c53e6e91186b80454b
GhadaSou <46688476+GhadaSou@users.noreply.github.com>
2020-07-15 13:00:22 -0400

    validation de payementForm

--- src/app/payment-page/payment-page.component.ts
+++ src/app/payment-page/payment-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { invalid } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-payment-page',
@@ -11,6 +12,10 @@ export class PaymentPageComponent implements OnInit {
   payementForm: FormGroup ;
   isMasterCard: boolean = false ;
   isVisa: boolean = false ;
+  name: boolean = false;
+  number: boolean;
+  date: boolean;
+  code: boolean;
 
   constructor(private formBuilder: FormBuilder, private router: Router) { }
 
@@ -21,9 +26,14 @@ export class PaymentPageComponent implements OnInit {
       code: ['', Validators.required],
       date: ['', Validators.required],
     })
+    this.payementForm.get('name').valueChanges.subscribe(
+      data => {
+        console.log('name', data)
+      }
+    )
     this.payementForm.get('number').valueChanges.subscribe(
       data => {
-        console.log('dataa changed', data)
+        console.log('nuber changed', data)
         if(data.substr(0, 1) === '5' ){
           console.log('ici master')
           this.isMasterCard = true;
@@ -35,13 +45,26 @@ export class PaymentPageComponent implements OnInit {
           this.isMasterCard = false;
         }
       }
-    ),err => {
-      console.log(err)
-    }
+    )
   }
 submit(){
+  if(this.payementForm.get('name').invalid){
+    console.log('nom non valide')
+    this.name = true ;
+  }else{
+    this.name = false;
+  }
+  if(this.payementForm.get('number').invalid){
+    this.number = true ;
+  }
+  if(this.payementForm.get('date').invalid){
+    this.date = true ;
+  }
+  if(this.payementForm.get('code').invalid){
+    this.code = true ;
+  }
   if (this.payementForm.valid){
-    //this.router.navigate(['/my-invoices']);
+    this.router.navigate(['/my-invoices']);
     console.log('valid')
 
   }else{
